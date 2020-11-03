f347ec177f820b87994c97c730449e5968d23efc
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2020-11-03 22:36:14 +0100

    Add wrapping function for git; Add `git root` function
    There is no possibility to run a function in the same shell,
    if defining an alias in .gitconfig. They all run in subshells.
    This is supoptimal if your function using `cd` for example.
    Bash functions however have a higher priority if they are self-defined.
    Defining a custom wrapper function with the same name works,
    as the function gets called instead of the command.
    MAKE SURE to use the command keyword in front of the wrapped function
    to avoid a recursive endless calling loop.
    First example using htis method is `git root`, which can now be called
    like a normal git command. (with a whitespace between git and root)

---
+++ bash/helpers/git_root.sh
@@ -0,0 +1,22 @@
+git(){
+	if [[ "$1" == "root" ]]; then 
+		shift;
+		git_root "$@"
+	else
+		command git "$@"
+	fi
+}
+git_root(){
+	toplevel_directory="$(git rev-parse --show-toplevel)"
+	echo "$toplevel_directory"
+	for arg in "$@";do
+		case $arg in
+			--move | -m )
+				cd "$toplevel_directory" || return
+				;;
+			* )
+				echo "Unknown argument"
+				;;
+		esac
+	done
+}
