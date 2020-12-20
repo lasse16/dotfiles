a5a3981722b9baf94cce6d46ae40d3db17457aa2
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2020-12-20 19:52:08 +0100

    Change to use default existing subdirectory path or passed argument
    Updated the implementation of the submodule adder, so that one can pass
    in the parent directory of the existing submodule folders.
    The plug manager for vim stores all plugins in a directory
    "vim/.vim/plugged", meaning it clones all repositories into folders
    under that parent directory. Previous iteration was meant to be called
    from the dotfile base folder, while this one now checks the passed in
    path for the existing repos.

--- bash/one-time-scripts/submodule_adder.sh
+++ bash/one-time-scripts/submodule_adder.sh
@@ -1,5 +1,6 @@
 input_file=$1
+preexisting_folder_path=${2:-"vim/.vim/plugged"}
 while read -r url directory; do 
 	echo "$url $directory"
-	git submodule add "$url" "vim/.vim/plugged/$directory"
+	git submodule add "$url" "$preexisting_folder_path/$directory"
 done < "$input_file"
