e195ef051a983001fdea5906fa68004ee0a947cb
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2021-05-15 22:19:32 +0200

    Disable not present ssh_keys

--- bash/helpers/ssh_agent.sh
+++ bash/helpers/ssh_agent.sh
@@ -10,10 +10,12 @@ function start_ssh_agent {
 
   run_ssh_env;
 
-  ssh-add ~/.ssh/id_rsa;
-  ssh-add ~/.ssh/id_gitlab;
-  ssh-add ~/.ssh/id_mafiasi;
-  ssh-add ~/.ssh/id_gitlab_uhh;
-  ssh-add ~/.ssh/id_rpi0;
+  # TODO Make a list or setting which keys to enable on which machine
+  # Alternatively enable every key in ssh directory (SECURITY)
+  #ssh-add ~/.ssh/id_rsa;
+  #ssh-add ~/.ssh/id_gitlab;
+  #ssh-add ~/.ssh/id_mafiasi;
+  #ssh-add ~/.ssh/id_gitlab_uhh;
+  #ssh-add ~/.ssh/id_rpi0;
 }
 
