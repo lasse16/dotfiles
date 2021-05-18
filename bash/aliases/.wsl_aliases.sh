e597fa8c9965df50ffc31499fe49cc1e4c4dfc3e
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2021-05-19 01:01:07 +0200

    Remove machine specific folder shortcuts

--- bash/aliases/.wsl_aliases.sh
+++ bash/aliases/.wsl_aliases.sh
@@ -1,8 +1,5 @@
 #WSL-specific aliases
-alias rep="cd /mnt/c/Users/Lasse/source/repos"
 alias docker="docker.exe"
 alias docker-compose="docker-compose.exe"
 alias start="wslview"
 alias vs="start *.sln"
-alias desktop="cd /mnt/c/Users/Lasse/Desktop"
-alias downloads="cd /mnt/c/Users/Lasse/Downloads"
