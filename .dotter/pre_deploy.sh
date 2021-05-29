533f0bc79b45ff5e7519ccd30123e11796672a0d
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2021-05-29 18:51:28 +0200

    Print {{environment.folder_shortcuts}} into file folder_shortcuts.txt

--- .dotter/pre_deploy.sh
+++ .dotter/pre_deploy.sh
@@ -4,3 +4,9 @@ export DOTFILES={{environment.dotfile_folder}}
 export CARGO_HOME={{rust.cargo_home}}
 export RUSTUP_HOME={{rust.rustup_home}}
 {{/if}}
+
+cat > "{{environment.dotfile_folder}}/folder_shortcuts" <<End-of-message
+{{~#each environment.folder_shortcuts}}
+{{this}}
+{{~/each}}
+End-of-message
