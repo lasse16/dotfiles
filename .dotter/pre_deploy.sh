4a6df8668132bb1ca3a006c02a12d14bf79faf8e
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2021-06-13 00:30:12 +0200

    Remove anything related to folder shortcuts
    Setting up controller shortcuts proves difficult.
    I am going to tackle the challenge later.
    For now, they are all deleted.

--- .dotter/pre_deploy.sh
+++ .dotter/pre_deploy.sh
@@ -4,9 +4,3 @@ export DOTFILES={{environment.dotfile_folder}}
 export CARGO_HOME={{rust.cargo_home}}
 export RUSTUP_HOME={{rust.rustup_home}}
 {{/if}}
-
-cat > "{{environment.dotfile_folder}}/folder_shortcuts" <<End-of-message
-{{~#each environment.folder_shortcuts}}
-{{this}}
-{{~/each}}
-End-of-message
