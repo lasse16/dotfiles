74a1522364d248bc5d63eef3acd9305aadd69475
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2021-06-15 01:11:24 +0200

    Install all vim plugins upon deployment

--- .dotter/pre_deploy.sh
+++ .dotter/pre_deploy.sh
@@ -4,3 +4,8 @@ export DOTFILES={{environment.dotfile_folder}}
 export CARGO_HOME={{rust.cargo_home}}
 export RUSTUP_HOME={{rust.rustup_home}}
 {{/if}}
+
+{{#if dotter.packages.vim}}
+# Open vim, install all plugins, immediately close windows afterwards
+vim -c PlugInstall -c qa
+{{/if}}
