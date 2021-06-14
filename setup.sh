eda9d37f46d28b88a60d986cd776c537e5e2980d
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2021-06-14 23:25:11 +0200

    Ask for $DOTFILES directory instead of hardcoding in setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-export DOTFILES=~/dotfiles
+
+ask_for_dotfile_repo(){
+read -e -r -i "$HOME/dotfiles" -p "Where are your dotfiles? " dotfile_candidate
+
+if [[ ! -d "$dotfile_candidate" ]]; then
+	echo "$?"
+	echo "Given dotfile folder does not exist... Aborting!"
+	exit
+fi
+}
+
 
 append_bash_history(){
 	if [ -f "$HOME/.bash_history" ] ; then
@@ -8,6 +18,9 @@ append_bash_history(){
 		rm "$HOME/.bash_history"
 	fi
 }
+
+# Ask for dotfile repo and set environment variable, Abort if either failed
+ask_for_dotfile_repo && export DOTFILES=$dotfile_candidate || exit
 
 # install packages with configurations
 cd "$DOTFILES" || exit
