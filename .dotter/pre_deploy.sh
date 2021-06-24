33f7be70eac00e2be30a8d36d5d0c62cd2f5b9e2
Lasse Haffke <lasse@haffke.me>
2021-06-24 14:59:55 +0200

    Remove environment variable setting from pre-deploy hook

--- .dotter/pre_deploy.sh
+++ .dotter/pre_deploy.sh
@@ -1,10 +1,3 @@
-export DOTFILES={{environment.dotfile_folder}}
-
-{{#if dotter.packages.rust}}
-export CARGO_HOME={{rust.cargo_home}}
-export RUSTUP_HOME={{rust.rustup_home}}
-{{/if}}
-
 {{#if dotter.packages.vim}}
 # Open vim, install all plugins, immediately close windows afterwards
 vim -c PlugInstall -c qa
