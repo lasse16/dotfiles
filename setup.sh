3c91b2b2602360180f1109e43a72ba2a4bd1a081
Lasse Haffke <lasse@haffke.me>
2021-07-13 23:52:55 +0200

    Remove nowunneccessary pre_deploy hook sourcing
    The sourcing was intended as a workaround for setting environment
    variables. I found no good way to set them this way, so I disabled
    them and now removed the sourcing of the rendered pre_deploy hook.

--- setup.sh
+++ setup.sh
@@ -27,8 +27,4 @@ cd "$DOTFILES" || exit
 append_bash_history;
 ./dotter deploy
 
-# shellcheck source=/home/lasse/dotfiles/.dotter/cache/.dotter/pre_deploy.sh
-# WORKAROUND Currently there is no way of setting environment variables from DOTTER itself
-# So, here I am actively sourcing the rendered template of the pre-deploy hook myself.
-source "$DOTFILES/.dotter/cache/.dotter/pre_deploy.sh"
 exec bash
