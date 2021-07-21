1a5f4d399a303cb370d34a58637ab3d8539c12ad
Lasse Haffke <lasse@haffke.me>
2021-07-21 18:24:53 +0200

    Replace relative link with absolute for bookmark_searcher

--- fzf/fzf_setup.sh
+++ fzf/fzf_setup.sh
@@ -1,2 +1,2 @@
 export FZF_DEFAULT_OPTS="--reverse --info=inline --border --height=80% --preview-window=:hidden --bind '?:toggle-preview' --preview 'cat {}' --bind 'ctrl-e:execute(echo {+} | xargs -o vim)' "
-source ./bookmark_searcher.sh
+. $DOTFILES/fzf/bookmark_searcher.sh
