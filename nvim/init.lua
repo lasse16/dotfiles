174715ae433152f0109b1cf459068b12ba751440
Lasse Haffke <lasse@haffke.me>
2021-08-18 02:55:03 +0200

    Setup  rust_analyzer

--- nvim/init.lua
+++ nvim/init.lua
@@ -30,4 +30,6 @@ vim.cmd('au TextYankPost * silent! lua vim.highlight.on_yank {on_visual=false}')
 
 vim.cmd(' let g:python3_host_prog = "/home/lasse/.local/share/nvim/python_environment/bin/python3"')
 
+require('lspconfig').rust_analyzer.setup({})
+
 print("INIT.LUA completed")
