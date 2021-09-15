7bc5a26256466db38b1dfe39f926c26b66c91fe5
Lasse Haffke <lasse@haffke.me>
2021-09-15 02:00:12 +0200

    Link ALE highlight groups to LSP highlight groups; Change ALE signs

--- nvim/plugin/ale.lua
+++ nvim/plugin/ale.lua
@@ -3,9 +3,27 @@ if vim.fn.has_key(vim.g.plugs, 'ale') then
 	-- Change ALEs display symbols
 	vim.g.ale_sign_warning ='🞄'
 	vim.g.ale_sign_error ='⬤'
+	vim.g.ale_sign_info ='!'
+	vim.g.ale_virtualtext_prefix ='◁◁◁◁'
 
 	vim.cmd( [[
 	let g:ale_fixers = { '*': ['remove_trailing_lines', 'trim_whitespace'] }
+]])
+
+	vim.cmd( [[
+
+	highlight link ALEError LspDiagnosticsDefaultError
+  highlight link ALEWarning LspDiagnosticsDefaultWarning
+  highlight link ALEInfo LspDiagnosticsDefaultInformation
+
+	highlight link ALEErrorSign LspDiagnosticsSignError
+	highlight link ALEWarningSign LspDiagnosticsSignWarning
+	highlight link ALEInfoSign LspDiagnosticsSignInformation
+
+  highlight link ALEVirtualTextError LspDiagnosticsVirtualTextError
+  highlight link ALEVirtualTextWarning LspDiagnosticsVirtualTextWarning
+  highlight link ALEVirtualTextInfo LspDiagnosticsVirtualTextInformation
+
 ]])
 
 	vim.g.ale_fix_on_save = 1
