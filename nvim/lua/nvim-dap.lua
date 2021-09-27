1624122c4444106cdcb7c51c3876047e8dfb7fb3
Lasse Haffke <lasse@haffke.me>
2021-09-27 03:18:24 +0200

    [WIP] Add DAP configuration for CodeLLDB and Rust

--- nvim/lua/nvim-dap.lua
+++ nvim/lua/nvim-dap.lua
@@ -32,3 +32,35 @@ dap.configurations.python = {
     end;
   },
 }
+
+-- Rust
+local codelldb_port=4000
+local codelldb_path='/home/lasse/.local/share/dap/lldb/adapter/codelldb'
+
+dap.adapters.lldb = function(callback, config)
+  -- NOT WORKING TODO was an attempt to start the server upon launching the debugee
+  -- os.execute(codelldb_path .. '--port ' .. config.port)
+  callback({ type = "server", host= '127.0.0.1', port = config.port })
+end
+
+dap.configurations.rust = {
+  {
+    -- The first three options are required by nvim-dap
+    type = 'lldb';
+    request = 'launch';
+    name = "Launch file";
+
+    -- Options below are for CodeLLDB
+    cwd = "${workspaceFolder}";
+    port = codelldb_port,
+    program = function()
+      local current_working_dir = vim.fn.getcwd()
+      local cwd_parent = vim.fn.fnamemodify(current_working_dir,':p:h:h')
+      local cwd_parent_name = vim.fn.fnamemodify(cwd_parent,':t')
+
+      return vim.fn.input("Path to executable: ", cwd_parent .. '/target/debug/' .. cwd_parent_name, "file")
+    end,
+    stopOnEntry = false,
+    args = {},
+      },
+}
