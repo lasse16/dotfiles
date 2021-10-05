28e9b70af075022cf1b29d4a763c05bc3064fb52
Lasse Haffke <lasse@haffke.me>
2021-10-05 23:50:02 +0200

    Remove zombie code

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -40,15 +40,6 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
--- MOVED TO ../plugin/
--- require('lspconfig').rust_analyzer.setup({
---   on_attach=on_attach,
---   capabilities=capabilities,
---   flags = {
---     debounce_text_changes = 200,
---   }
--- })
-
 print("LSP config finished")
 
 config = {
