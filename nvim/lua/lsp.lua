8804059a32ef239eed182b9fe1652d8bc665f229
Lasse Haffke <lasse@haffke.me>
2021-12-29 22:12:03 +0100

    Remove references to ale-diagnostic; Diagnostics are handled by LSP

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -27,11 +27,9 @@ local on_attach = function(client, bufnr)
 	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border })
 	vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border })
 
-	-- Disable LSP diagnostics as ALE handles displaying those
-	require("nvim-ale-diagnostic")
 	vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-		underline = false,
-		virtual_text = false,
+		underline = true,
+		virtual_text = true,
 		signs = true,
 		update_in_insert = false,
 	})
