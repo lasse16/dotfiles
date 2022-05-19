c1af4c12d3d33e75036e2131248a6fdcd9de28ad
Lasse Haffke <lasse@haffke.me>
2022-05-19 23:17:58 +0200

    Swap colorizer plugin to supported fork of nvim-colorizer

--- nvim/lua/packer-manager.lua
+++ nvim/lua/packer-manager.lua
@@ -17,7 +17,7 @@ packer = require("packer").startup({
 		use("junegunn/vim-easy-align")
 		use("tpope/vim-fugitive")
 		use("nvim-lualine/lualine.nvim")
-		use("ap/vim-css-color")
+		use("NvChad/nvim-colorizer.lua")
 		use("numToStr/Comment.nvim")
 		use("tpope/vim-surround")
 		use("wellle/targets.vim")
