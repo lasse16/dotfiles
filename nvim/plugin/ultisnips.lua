f92692d153ffb30e27f33b24855997acc1342c5b
Lasse Haffke <lasse@haffke.me>
2022-05-20 22:18:05 +0200

    Delete configuration from old snippet engine

--- nvim/plugin/ultisnips.lua
+++
@@ -1,5 +0,0 @@
-if require("utils").installed("ultisnips") then
-	--  	 Unbind this as per default it is bound to <Tab> which conflicts with auto
-	--  	 completion
-	vim.g.UltiSnipsExpandTrigger = "<Nop>"
-end
