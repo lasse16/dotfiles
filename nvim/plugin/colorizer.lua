7f2305b0567bfcb0b1e65801d56f4f34c94882b1
Lasse Haffke <lasse@haffke.me>
2022-05-20 17:13:40 +0200

    Attach colprizer to scss files too

--- nvim/plugin/colorizer.lua
+++ nvim/plugin/colorizer.lua
@@ -1,2 +1,2 @@
 -- https://github.com/norcalli/nvim-colorizer.lua
-require("colorizer").setup({ "html", "css", "yaml", "toml", "markdown" }, { css = true })
+require("colorizer").setup({ "html", "css", "scss", "yaml", "toml", "markdown" }, { css = true })
