960c691e13a08d8edcf70be5aab8e639eff3fd50
Lasse Haffke <lasse@haffke.me>
2022-05-31 17:07:15 +0200

    Remove all pritning upon bash loading

--- bash/keybindings.sh
+++ bash/keybindings.sh
@@ -1,7 +1,7 @@
+#! /bin/bash
+
 # Open file found via fzf in nvim
 bind -x '"\C-f": "nvim $(fzf)"'
 
 # Open browser window TODO hardcoded to firefox
 bind -x '"\C-b": "firefox -w"'
-
-echo "keybindings set"
