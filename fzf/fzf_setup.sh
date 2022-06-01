f77042dccc7422915f9ccf8a2ee1e737895f4f9e
Lasse Haffke <lasse@haffke.me>
2022-06-01 19:57:47 +0200

    Remove fzf configuration as default is good enough

--- fzf/fzf_setup.sh
+++
@@ -1,2 +0,0 @@
-export FZF_DEFAULT_OPTS="--reverse --info=inline --border --height=80% --preview-window=:hidden --bind '?:toggle-preview' --preview 'cat {}' --bind 'ctrl-e:execute(echo {+} | xargs -o vim)' "
-. $DOTFILES/fzf/bookmark_searcher.sh
