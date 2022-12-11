84a80f8f2aab6a4ef3c5a4148d7c2ec6252d8178
Lasse Haffke <lasse@haffke.me>
2022-12-12 00:41:52 +0100

    Remove unnecessary DAP-UI open call

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -71,7 +71,7 @@ end
 
 function M.set_debugger_keymappings()
 	vim.cmd([[
-		nnoremap <silent> <F5> :lua require'dap'.continue() require'dapui'.open()<CR>
+		nnoremap <silent> <F5> :lua require'dap'.continue()<CR>
 		nnoremap <silent> <F10> :lua require'dap'.step_over()<CR>
 		nnoremap <silent> <F11> :lua require'dap'.step_into()<CR>
 		nnoremap <silent> <F12> :lua require'dap'.step_out()<CR>
