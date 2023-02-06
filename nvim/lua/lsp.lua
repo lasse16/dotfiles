68c59f2e6773bb2484c45057980ae42c93d40022
Lasse Haffke <lasse@haffke.me>
2023-02-06 01:17:23 +0100

    Add neovim specific settings to sumneko lsp

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -80,11 +80,30 @@ end
 
 local lsp_config = require("lspconfig")
 
-lsp_config.sumneko_lua.setup(	 {
+lsp_config.sumneko_lua.setup({
 	cmd = { "/home/lasse/.local/share/lua-lsp/bin/lua-language-server" },
 	on_attach = config.on_attach,
 	capabilities = config.capabilities,
+	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+				version = 'LuaJIT',
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { 'vim' },
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+		},
 	}
-)
+})
 
 return config
