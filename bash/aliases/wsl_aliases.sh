44cffc52262cd5013953fd42133a73206bf26277
Lasse Haffke <lasse@haffke.me>
2023-02-14 11:23:21 +0100

    Move kubectl alias into wsl-specific aliases

--- bash/aliases/wsl_aliases.sh
+++ bash/aliases/wsl_aliases.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
 #WSL-specific aliases
 alias docker="docker.exe"
 alias docker-compose="docker-compose.exe"
 alias start="wslview"
 alias vs="start *.sln"
 alias xdg-open=wslview
+alias kubectl="kubectl.exe"
