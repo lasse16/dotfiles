d8b9600efdfefb51e450f9da2c52656758b6e1c0
Lasse Haffke <lasse@haffke.me>
2023-02-15 02:52:36 +0100

    Make fzf setup sh-complient

--- fzf/fzf_setup.sh
+++ fzf/fzf_setup.sh
@@ -3,8 +3,9 @@
 export DOTFILES_FZF=${DOTFILES_FZF:="$DOTFILES/fzf"}
 export FZF_DEFAULT_COMMAND='fd --hidden -E ".git"'
 
-source "$DOTFILES_FZF/aliases.sh"
-source "$DOTFILES_FZF/marks-fzf.sh"
-source "$DOTFILES_FZF/pkgsearch-fzf.sh"
-source "$DOTFILES_FZF/irg-fzf.sh"
-source "$DOTFILES_FZF/tmux-url-parsing-fzf.sh"
+# Uncomment when sourcing directly 
+# source "$DOTFILES_FZF/aliases.sh"
+# source "$DOTFILES_FZF/marks-fzf.sh"
+# source "$DOTFILES_FZF/pkgsearch-fzf.sh"
+# source "$DOTFILES_FZF/irg-fzf.sh"
+# source "$DOTFILES_FZF/tmux-url-parsing-fzf.sh"
