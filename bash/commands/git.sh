b9f7d498ebad6a21a12b898dc880cff9c2c64a0a
Lasse Haffke <lasse@haffke.me>
2023-04-03 18:01:21 +0200

    Add wrapper for git_add_from_modified

--- bash/commands/git.sh
+++ bash/commands/git.sh
@@ -4,6 +4,9 @@ git() {
 	if [[ "$1" == "root" ]]; then
 		shift
 		git_root "$@"
+	elif [[ "$1" == "add" ]] && [[ "$2" == "--from-modified" ]]; then
+		shift 2
+		git_add_from_modified "$@"
 	else
 		command git "$@"
 	fi
