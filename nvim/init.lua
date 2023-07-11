68eec16ab881c318cb760eb9ea17f85315243549
Lasse Haffke <lasse@haffke.me>
2023-07-11 19:58:38 +0200

    feat: bootstrap lazy.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,20 @@
 vim.cmd("au TextYankPost * silent! lua vim.highlight.on_yank {on_visual=false}")
 
+-- bootsstrap lazy
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+--
 vim.g.packpath = vim.g.runtimepath
 require("plugins")
 require("clipboard")
