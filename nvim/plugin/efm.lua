7d29f7d0b765abf3e19c399408f28b61597ba163
Lasse Haffke <lasse@haffke.me>
2023-12-21 17:32:56 +0100

    feat: add stylua; fix: remove unused linters and formatters

--- nvim/plugin/efm.lua
+++ nvim/plugin/efm.lua
@@ -2,9 +2,12 @@ local lsp = require("lsp")
 
 local shellcheck = require('efmls-configs.linters.shellcheck')
 local shfmt = require('efmls-configs.formatters.shfmt')
+local stylua = require('efmls-configs.formatters.stylua')
+
 
 local languages = {
 	bash = { shellcheck, shfmt },
+	lua = { stylua },
 }
 
 require('lspconfig').efm.setup({
@@ -23,16 +26,3 @@ require('lspconfig').efm.setup({
 		languages = languages,
 	}
 })
--- 		Leftovers from null_ls.lua
--- 		formatters.stylua,
--- 		formatters.black.with({ command = neovim_python_env .. "black" }),
--- 		formatters.shfmt,
--- 		formatters.prettier.with({ disabled_filetypes = { "scss", "css" } }),
--- 		formatters.stylelint,
--- 		linters.shellcheck,
--- 		linters.flake8.with({ command = neovim_python_env .. "flake8", name = "flake8" }),
--- 		linters.pylint.with({ command = neovim_python_env .. "pylint", name = "pylint" }),
--- 		linters.proselint,
--- 		linters.stylelint,
--- 		linters.eslint_d,
--- 		linters.golangci_lint,
