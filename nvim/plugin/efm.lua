0d512f0a9eef742083a98361dee8827141498142
Lasse Haffke <lasse@haffke.me>
2023-12-21 18:09:25 +0100

    feat: add additional tools

--- nvim/plugin/efm.lua
+++ nvim/plugin/efm.lua
@@ -1,16 +1,21 @@
 local lsp = require("lsp")
 
-local shellcheck = require('efmls-configs.linters.shellcheck')
-local shfmt = require('efmls-configs.formatters.shfmt')
-local stylua = require('efmls-configs.formatters.stylua')
+local shellcheck = require("efmls-configs.linters.shellcheck")
+local vale = require("efmls-configs.linters.vale")
+local yamllint = require("efmls-configs.linters.yamllint")
 
+local stylua = require("efmls-configs.formatters.stylua")
+local shfmt = require("efmls-configs.formatters.shfmt")
+local prettier_d = require("efmls-configs.formatters.prettier_d")
 
 local languages = {
 	bash = { shellcheck, shfmt },
 	lua = { stylua },
+	markdown = { vale, prettier_d },
+	yaml = { prettier_d, yamllint },
 }
 
-require('lspconfig').efm.setup({
+require("lspconfig").efm.setup({
 	init_options = {
 		documentFormatting = true,
 		documentRangeFormatting = true,
@@ -24,5 +29,5 @@ require('lspconfig').efm.setup({
 	filetypes = vim.tbl_keys(languages),
 	settings = {
 		languages = languages,
-	}
+	},
 })
