c9f7aa2acb69f5270d185cd48cd3b68cc7ad3e56
Lasse Haffke <lasse@haffke.me>
2024-01-19 13:37:35 +0100

    fix: Remove hardcoded firefox and rely on $BROWSER var

--- bash/keybindings.sh
+++ bash/keybindings.sh
@@ -3,5 +3,5 @@
 # Open file found via fzf in nvim
 bind -x '"\C-p": "nvim $(fzf)"'
 
-# Open browser window TODO hardcoded to firefox
-bind -x '"\C-b": "firefox -w"'
+# Open browser window
+bind -x '"\C-b": "$BROWSER -w"'
