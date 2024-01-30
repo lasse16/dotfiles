d40d3ae5361bce253e5597af429a3b12bb88b5e3
Lasse Haffke <lasse@haffke.me>
2024-01-30 14:42:46 +0100

    feat: add better interactive ripgrep script

--- fzf/fzf_setup.sh
+++ fzf/fzf_setup.sh
@@ -4,10 +4,10 @@ export DOTFILES_FZF=${DOTFILES_FZF:="$DOTFILES/fzf"}
 export FZF_DEFAULT_COMMAND='fd --hidden -E ".git"'
 
 # Uncomment when sourcing directly 
-# source "$DOTFILES_FZF/aliases.sh"
-# source "$DOTFILES_FZF/marks-fzf.sh"
-# source "$DOTFILES_FZF/pkgsearch-fzf.sh"
-# source "$DOTFILES_FZF/irg-fzf.sh"
-# source "$DOTFILES_FZF/tmux-url-parsing-fzf.sh"
-# source "$DOTFILES_FZF/fzf-tmux.sh"
-# source "$DOTFILES_FZF/key-bindings.bash"
+source "$DOTFILES_FZF/aliases.sh"
+source "$DOTFILES_FZF/marks-fzf.sh"
+source "$DOTFILES_FZF/pkgsearch-fzf.sh"
+source "$DOTFILES_FZF/tmux-url-parsing-fzf.sh"
+source "$DOTFILES_FZF/fzf-tmux.sh"
+source "$DOTFILES_FZF/key-bindings.bash"
+source "$DOTFILES_FZF/interactive-ripgrep.bash"
