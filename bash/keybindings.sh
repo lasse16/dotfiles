be7045fa72aace678d0fcf969d4313fb76dba38b
Lasse Haffke <lasse@haffke.me>
2024-01-30 13:54:55 +0100

    feat: add new bash keybindings

--- bash/keybindings.sh
+++ bash/keybindings.sh
@@ -5,3 +5,9 @@ bind -x '"\C-p": "file=$(fzf) && $EDITOR $file"'
 
 # Open browser window
 bind -x '"\C-b": "$BROWSER -w"'
+
+# Open URL from in pane
+bind -x '"\C-f": "tmux_url_parse"'
+
+# Open URL from in pane
+bind -x '"\C-s": "git status"'
