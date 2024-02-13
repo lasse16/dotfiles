dccea1a14f7b7d88fb95a21da11c94aa41036b94
Lasse Haffke <lasse.haffke@lhsystems.com>
2024-02-13 11:20:12 +0100

    fix: remove outdated pre_deploy hook

--- .dotter/pre_deploy.sh
+++
@@ -1,9 +0,0 @@
-{{#if dotter.packages.vim}}
-# Open vim, install all plugins, immediately close windows afterwards
-vim -c PlugInstall -c qa
-{{/if}}
-
-{{#if dotter.packages.fzf }}
-# Source  environment variable for FZF options
-. fzf/fzf_setup.sh
-{{/if}}
