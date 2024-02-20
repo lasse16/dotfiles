1b9223465e9ca9ef143f86487957e33d278349cf
Lasse Haffke <lasse.haffke@lhsystems.com>
2024-02-20 19:38:24 +0100

    feat: add minimal commander config

---
+++ nvim/plugin/commander.lua
@@ -0,0 +1,31 @@
+require('commander').setup({
+	prompt_title = "Commands",
+	components = {
+		"DESC",
+		"KEYS",
+		"CAT",
+	},
+	sort_by = {
+		"DESC",
+		"KEYS",
+		"CAT",
+		"CMD"
+	},
+	integration = {
+		telescope = {
+			enable = true,
+		},
+		lazy = {
+			enable = true,
+			set_plugin_name_as_cat = true
+		}
+	}
+})
+
+require("commander").add({
+	{
+		desc = "Open command palette",
+		cmd = require("commander").show,
+		keys = { "n", "<C-o>" },
+	}
+})
