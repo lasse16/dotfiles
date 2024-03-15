1ff508f58c0a68e818f937a1f6f65760ded3dedf
Lasse Haffke <lasse@haffke.me>
2024-03-16 00:18:13 +0100

    fix: remove unused just treesitter parser

--- nvim/lua/treesitter.lua
+++ nvim/lua/treesitter.lua
@@ -4,12 +4,3 @@ require("nvim-treesitter.configs").setup({
 		additional_vim_regex_highlighting = false,
 	},
 })
--- Add non-standard parser
-require("nvim-treesitter.parsers").get_parser_configs().just = {
-	install_info = {
-		url = "https://github.com/IndianBoy42/tree-sitter-just", -- local path or git repo
-		files = { "src/parser.c", "src/scanner.cc" },
-		branch = "main",
-	},
-	maintainers = { "@IndianBoy42" },
-}
