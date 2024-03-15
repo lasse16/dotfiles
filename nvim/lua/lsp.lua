482db88403cc18e50d78cabcc11e733bdbd51d63
Lasse Haffke <lasse@haffke.me>
2024-03-16 00:14:36 +0100

    fix: extend capabilities in new kickstart.nvim way

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -37,7 +37,7 @@ end
 
 -- Setup completion engine
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
 config = {
 	on_attach = on_attach,
