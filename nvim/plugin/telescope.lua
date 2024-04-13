839ef7a80edff53fd93b533dff267621d18dac78
Lasse Haffke <lasse@haffke.me>
2024-04-13 18:40:07 +0200

    feat: enable context-aware bibtex search

--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -15,6 +15,10 @@ telescope.setup({
 			override_generic_sorter = true, -- override the generic sorter
 			override_file_sorter = true, -- override the file sorter
 			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+		},
+
+		bibtex = {
+			context = true, -- parse file for bibliography: field
 		}
 	}
 })
