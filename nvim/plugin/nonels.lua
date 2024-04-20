da8c3c1f9f476b6f77262ef6175d946d51e76c29
Lasse Haffke <lasse@haffke.me>
2024-04-20 23:13:11 +0200

    feat: add gitsigns, treesiter-refactor, vale, alejandra, yamlfmt

--- nvim/plugin/nonels.lua
+++ nvim/plugin/nonels.lua
@@ -3,6 +3,11 @@ require("null-ls").setup({
 	sources = {
 		builtins.diagnostics.actionlint,
 		builtins.diagnostics.yamllint,
+		builtins.code_actions.gitsigns,
+		builtins.code_actions.refactoring,
+		builtins.diagnostics.vale.with({ extra_filetypes = { "quarto" } }),
+		builtins.formatting.alejandra,
+		builtins.formatting.yamlfmt,
 	},
-	update_in_insert = false
+	update_in_insert = false,
 })
