b4ac84dddc6dcebf9d8c81a32b18513cbcad4ba4
Lasse Haffke <lasse@haffke.me>
2024-07-07 13:43:22 +0200

    feat(nvim): install markdown tooling

---
+++ nvim/lua/plugins/markdown.lua
@@ -0,0 +1,10 @@
+return {
+	"tadmccorkle/markdown.nvim",
+	ft = "markdown", -- or 'event = "VeryLazy"'
+	opts = {
+		mappings = false,
+		on_attach = function(bufnr)
+			require("mappings").setup_markdown_mappings(bufnr)
+		end
+	},
+}
