850002439de8cc94b18f05e615f0463cdf439ab1
Lasse Haffke <lasse@haffke.me>
2024-07-09 19:24:48 +0200

    feat(fzf): add preview to github repo selection

--- fzf/github_select_repo.bash
+++ fzf/github_select_repo.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 gh_select_repos() {
-	gh org list | xargs -l1 gh repo list | cut -f1 | fzf 
+	gh org list | xargs -l1 gh repo list | cut -f1 | fzf --preview "gh repo view {}" --ansi
 }
 
 export -f gh_select_repos
