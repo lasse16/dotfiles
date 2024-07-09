399d7dd5ffeb21ccb4a745034264bd46e154a61f
Lasse Haffke <lasse@haffke.me>
2024-07-09 19:40:38 +0200

    fix(nvim): formatting; make markdown file a list of plugins

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,10 +1,12 @@
 return {
-	"tadmccorkle/markdown.nvim",
-	ft = "markdown", -- or 'event = "VeryLazy"'
-	opts = {
-		mappings = false,
-		on_attach = function(bufnr)
-			require("mappings").setup_markdown_mappings(bufnr)
-		end
+	{
+		"tadmccorkle/markdown.nvim",
+		ft = "markdown", -- or 'event = "VeryLazy"'
+		opts = {
+			mappings = false,
+			on_attach = function(bufnr)
+				require("mappings").setup_markdown_mappings(bufnr)
+			end
+		},
 	},
 }
