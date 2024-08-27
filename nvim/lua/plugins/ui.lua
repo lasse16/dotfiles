23eae4751c362520c4959a6e765af85d2399f525
Lasse Haffke <lasse@haffke.me>
2024-08-27 20:11:33 +0200

    feat(nvim): add ui-focussed plugins

---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,29 @@
+return {
+	{ "nvim-lualine/lualine.nvim" },
+	{ "stevearc/dressing.nvim" },
+	{ "folke/trouble.nvim",       branch = "main" },
+	{
+		"folke/zen-mode.nvim",
+		opts = {
+			window = {
+				options = {
+					number = true,
+					relativenumber = true,
+					conceallevel = 0
+				},
+			},
+			plugins = {
+				twilight = { enabled = false }, -- enable to start Twilight when zen mode opens
+				gitsigns = { enabled = false }, -- disables git signs
+				tmux = { enabled = false }, -- disables the tmux statusline
+				-- this will change the font size on alacritty when in zen mode
+				-- requires  Alacritty Version 0.10.0 or higher
+				-- uses `alacritty msg` subcommand to change font size
+				alacritty = {
+					enabled = true,
+					font = "14", -- font size
+				},
+			},
+		},
+	},
+}
