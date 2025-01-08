6f9687066629a14bc8fbe5ce9bc1e0fc248cfcac
Lasse Haffke <lasse@haffke.me>
2025-01-08 18:12:29 +0100

    feat(nvim-snacks): setup basic snacks configuration

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -32,5 +32,23 @@ return {
 	{ "numToStr/Navigator.nvim" },
 	{ "rhysd/clever-f.vim" },
 	{ "mrjones2014/smart-splits.nvim" },
-	{"mfussenegger/nvim-dap-python"},
+	{ "mfussenegger/nvim-dap-python" },
+	{
+		---@module 'snacks'
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		---@type snacks.Config
+		opts = {
+			bigfile = { enabled = true },
+			dashboard = { enabled = true },
+			indent = { enabled = true },
+			input = { enabled = true },
+			notifier = { enabled = true },
+			quickfile = { enabled = true },
+			scroll = { enabled = true },
+			statuscolumn = { enabled = true },
+			words = { enabled = true },
+		},
+	},
 }
