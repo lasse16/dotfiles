c7774104972d3ea9e2d5c121dfd346a84cf97728
Lasse Haffke <lasse@haffke.me>
2025-01-08 02:46:32 +0100

    feat(nvim-cmp): change keymaps to closer resemble nvim-cmp maps

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -2,7 +2,8 @@ return {
 	'saghen/blink.cmp',
 	version = '*',
 	opts = {
-		keymap = { preset = 'default' },
+		-- https://cmp.saghen.dev/configuration/keymap#enter
+		keymap = { preset = 'enter' },
 		appearance = {
 			use_nvim_cmp_as_default = true,
 			nerd_font_variant = 'mono'
