b73986ae139677294b43377b150da3ddec816d95
Lasse Haffke <lasse@haffke.me>
2025-01-08 02:45:30 +0100

    feat(nvim-cmp): add luasnip support

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -8,13 +8,23 @@ return {
 			nerd_font_variant = 'mono'
 		},
 		sources = {
-			default = { 'lsp', 'path', 'snippets', 'buffer' },
+			default = { 'lsp', 'path', 'luasnip', 'snippets', 'buffer' },
 		},
 		completion = {
 			menu = { border = 'rounded' },
 			documentation = { window = { border = 'rounded' } },
 		},
 		signature = { window = { border = 'rounded' }, enabled = true },
+		snippets = {
+			expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
+			active = function(filter)
+				if filter and filter.direction then
+					return require('luasnip').jumpable(filter.direction)
+				end
+				return require('luasnip').in_snippet()
+			end,
+			jump = function(direction) require('luasnip').jump(direction) end,
+		},
 	},
 	opts_extend = { "sources.default" }
 }
