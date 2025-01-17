8acace4179fd4bae76be036813b287d1bbfaac9f
Lasse Haffke <lasse@haffke.me>
2025-01-17 16:44:48 +0100

    feat(nvim-help): open help in floating window

--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -9,6 +9,41 @@ telescope.setup({
 			},
 		},
 	},
+	pickers = {
+		help_tags = {
+			mappings = {
+				i = {
+					["<CR>"] = function(prompt_bufnr)
+						local selection = require("telescope.actions.state").get_selected_entry()
+						require("telescope.actions").close(prompt_bufnr)
+						if selection then
+							vim.api.nvim_command("help " .. selection.value)
+							-- Move the help window to a floating window
+							vim.api.nvim_command("wincmd J")
+							local win = vim.api.nvim_get_current_win()
+							local buf = vim.api.nvim_get_current_buf()
+							local width = math.floor(vim.o.columns * 0.9)
+							local height = math.floor(vim.o.lines * 0.9)
+							local row = math.floor((vim.o.lines - height) / 2)
+							local col = math.floor((vim.o.columns - width) / 2)
+							vim.api.nvim_open_win(buf, true, {
+								relative = 'editor',
+								title = " Help ",
+								title_pos = "center",
+								width = width,
+								height = height,
+								row = row,
+								col = col,
+								style = 'minimal',
+								border = 'rounded',
+							})
+							vim.api.nvim_win_close(win, true)
+						end
+					end
+				}
+			}
+		}
+	},
 	extensions = {
 		fzf = {
 			fuzzy = true, -- false will only do exact matching
