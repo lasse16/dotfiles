e2573f4ad46e17b9e9cbaaf22a5e846bc469ea7e
Lasse Haffke <lasse@haffke.me>
2025-01-17 17:26:25 +0100

    fix(nvim-help): avoid window commands when opening floating help

--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -17,16 +17,12 @@ telescope.setup({
 						local selection = require("telescope.actions.state").get_selected_entry()
 						require("telescope.actions").close(prompt_bufnr)
 						if selection then
-							vim.api.nvim_command("help " .. selection.value)
-							-- Move the help window to a floating window
-							vim.api.nvim_command("wincmd J")
-							local win = vim.api.nvim_get_current_win()
 							local buf = vim.api.nvim_get_current_buf()
 							local width = math.floor(vim.o.columns * 0.9)
 							local height = math.floor(vim.o.lines * 0.9)
 							local row = math.floor((vim.o.lines - height) / 2)
 							local col = math.floor((vim.o.columns - width) / 2)
-							vim.api.nvim_open_win(buf, true, {
+							local win = vim.api.nvim_open_win(buf, true, {
 								relative = 'editor',
 								title = " Help ",
 								title_pos = "center",
@@ -37,7 +33,9 @@ telescope.setup({
 								style = 'minimal',
 								border = 'rounded',
 							})
-							vim.api.nvim_win_close(win, true)
+							local bufnr = vim.api.nvim_win_get_buf(win)
+							vim.api.nvim_set_option_value('buftype', 'help', { buf = bufnr })
+							vim.api.nvim_command("help " .. selection.value)
 						end
 					end
 				}
