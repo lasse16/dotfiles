ed39e37361dbb2f0b2e3eb52f08d5be8ed8a4fa7
Lasse Haffke <lasse@haffke.me>
2025-01-23 13:16:26 +0100

    feat(nvim-snacks): set basic snacks keymaps

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -180,4 +180,39 @@ function M.setup_markdown_mappings(bufnr)
 	add_mappings_from_table(markdown_mappings)
 end
 
+local Snacks = require('snacks')
+local snacks_mappings = {
+	{ "<leader>z",  function() Snacks.zen() end,                   desc = "Toggle Zen Mode" },
+	{ "<leader>Z",  function() Snacks.zen.zoom() end,              desc = "Toggle Zoom" },
+	{ "<leader>.",  function() Snacks.scratch() end,               desc = "Toggle Scratch Buffer" },
+	{ "<leader>S",  function() Snacks.scratch.select() end,        desc = "Select Scratch Buffer" },
+	{ "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
+	{ "<leader>gB", function() Snacks.gitbrowse() end,             desc = "Git Browse",               mode = { "n", "v" } },
+	{ "<leader>un", function() Snacks.notifier.hide() end,         desc = "Dismiss All Notifications" },
+	{ "<leader>t",      function() Snacks.terminal() end,              desc = "Toggle Terminal" },
+	{
+		"<leader>N",
+		desc = "Neovim News",
+		function()
+			Snacks.win({
+				file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+				width = 0.6,
+				height = 0.6,
+				wo = {
+					spell = false,
+					wrap = false,
+					signcolumn = "yes",
+					statuscolumn = " ",
+					conceallevel = 3,
+				},
+			})
+		end,
+	}
+}
+
+M.Snacks_keys = snacks_mappings
+function M.set_snacks_keys()
+	add_mappings_from_table(M.Snacks_keys)
+end
+
 return M
