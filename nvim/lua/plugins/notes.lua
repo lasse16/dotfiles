835507cb5be41ea93b7234370bf3881abf5c2727
Lasse Haffke <lasse@haffke.me>
2025-04-10 01:00:17 +0200

    feat(nvim-plugins): add zk configuration

--- nvim/lua/plugins/notes.lua
+++ nvim/lua/plugins/notes.lua
@@ -1,15 +1,17 @@
 return {
 	{
 		"zk-org/zk-nvim",
-		opts = {
-			picker = "telescope",
-			auto_attach = {
-				enabled = true,
-				filetypes = { "markdown" },
-			},
-			lsp = {
-				on_attach = require("lsp").on_attach,
-			}
-		}
+		config = function()
+			require("zk").setup({
+				picker = "telescope",
+				auto_attach = {
+					enabled = true,
+					filetypes = { "markdown" },
+				},
+				lsp = {
+					on_attach = require("lsp").on_attach,
+				}
+			})
+		end,
 	}
 }
