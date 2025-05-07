cbc986659c54ba5cb389598a55c5bc6e9aedb9b9
Lasse Haffke <lasse@haffke.me>
2025-05-07 23:40:37 +0200

    feat(nvim-lualine): add section to display schema if present

--- nvim/plugin/lualine.lua
+++ nvim/plugin/lualine.lua
@@ -1,3 +1,16 @@
 require("lualine").setup({
-	options = { theme = "onedark" },
+    options = { theme = "onedark" },
+    sections = {
+        lualine_c = { "filename",
+            {
+                function()
+                    local schema = require("schema-companion.context").get_buffer_schema()
+                    return ("%s"):format(schema)
+                end,
+                cond = function()
+                    return package.loaded["schema-companion"] ~= nil
+                end,
+            },
+        },
+    },
 })
