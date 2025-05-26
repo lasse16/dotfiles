222a1fd936e5bd3e9937e9ab909e5761e168c686
Lasse Haffke <lasse@haffke.me>
2025-05-26 17:07:42 +0200

    feat(fzf): source new alias directory

--- fzf/fzf_setup.sh
+++ fzf/fzf_setup.sh
@@ -12,3 +12,6 @@ source "$DOTFILES_FZF/fzf-tmux.sh"
 source "$DOTFILES_FZF/key-bindings.bash"
 source "$DOTFILES_FZF/interactive-ripgrep.bash"
 source "$DOTFILES_FZF/github_select_repo.bash"
+
+# Add subcommands by sourcing files with specific names
+pathadd "$DOTFILES_FZF/kubernetes/"
