61874bfdd0562faed0dd6638979307121c630ff5
Lasse Haffke <lasse@haffke.me>
2025-06-25 01:03:50 +0200

    feat(nvim): add mcphub for integration with MCP tools

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,8 +1,20 @@
 return {
     "olimorris/codecompanion.nvim",
-    opts = {},
+    opts = {
+        extensions = {
+            mcphub = {
+                callback = "mcphub.extensions.codecompanion",
+                opts = {
+                    show_result_in_chat = true,                       -- Show mcp tool results in chat
+                    make_vars = true,                                 -- Convert resources to #variables
+                    make_slash_commands = true,                       -- Add prompts as /slash commands
+                }
+            }
+        }
+    },
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-treesitter/nvim-treesitter",
+        "ravitemer/mcphub.nvim"
     },
 }
