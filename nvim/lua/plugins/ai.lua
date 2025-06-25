3d769d02cb06af8e4031898cec8a39231a5c1686
Lasse Haffke <lasse@haffke.me>
2025-06-25 16:52:49 +0200

    feat(nvim): setup codecompanion mappings

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,21 +1,25 @@
 return {
-    "olimorris/codecompanion.nvim",
-    opts = {
-        extensions = {
-            mcphub = {
-                callback = "mcphub.extensions.codecompanion",
-                opts = {
-                    show_result_in_chat = true,                       -- Show mcp tool results in chat
-                    make_vars = true,                                 -- Convert resources to #variables
-                    make_slash_commands = true,                       -- Add prompts as /slash commands
-                }
-            }
-        }
-            "github/copilot.vim",
-    },
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-treesitter/nvim-treesitter",
-        "ravitemer/mcphub.nvim"
-    },
+	"olimorris/codecompanion.nvim",
+	opts = {
+		extensions = {
+			mcphub = {
+				callback = "mcphub.extensions.codecompanion",
+				opts = {
+					show_result_in_chat = true, -- Show mcp tool results in chat
+					make_vars = true, -- Convert resources to #variables
+					make_slash_commands = true, -- Add prompts as /slash commands
+				},
+			},
+		},
+	},
+	config = function(_, opts)
+		require("codecompanion").setup(opts)
+		require("mappings").setup_code_companion_mappings()
+	end,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"github/copilot.vim",
+		"nvim-treesitter/nvim-treesitter",
+		"ravitemer/mcphub.nvim",
+	},
 }
