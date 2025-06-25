ce2a231b6caab0957a86773b1a6590cea2185a15
Lasse Haffke <lasse@haffke.me>
2025-06-26 00:26:32 +0200

    fix(nvim): call out to mcphub for proper setup

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,25 +1,32 @@
 return {
-	"olimorris/codecompanion.nvim",
-	opts = {
-		extensions = {
-			mcphub = {
-				callback = "mcphub.extensions.codecompanion",
-				opts = {
-					show_result_in_chat = true, -- Show mcp tool results in chat
-					make_vars = true, -- Convert resources to #variables
-					make_slash_commands = true, -- Add prompts as /slash commands
+	{
+
+		"olimorris/codecompanion.nvim",
+		opts = {
+			extensions = {
+				mcphub = {
+					callback = "mcphub.extensions.codecompanion",
+					opts = {
+						show_result_in_chat = true, -- Show mcp tool results in chat
+						make_vars = true, -- Convert resources to #variables
+						make_slash_commands = true, -- Add prompts as /slash commands
+					},
 				},
 			},
 		},
+		config = function(_, opts)
+			require("codecompanion").setup(opts)
+			require("mappings").setup_code_companion_mappings()
+		end,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"github/copilot.vim",
+			"nvim-treesitter/nvim-treesitter",
+			"ravitemer/mcphub.nvim",
+		},
 	},
-	config = function(_, opts)
-		require("codecompanion").setup(opts)
-		require("mappings").setup_code_companion_mappings()
-	end,
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"github/copilot.vim",
-		"nvim-treesitter/nvim-treesitter",
+	{
 		"ravitemer/mcphub.nvim",
+		opts = {},
 	},
 }
