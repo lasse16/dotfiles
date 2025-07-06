c2f3a3089818ff45c2dc7b10ab12a4af208110de
Lasse Haffke <lasse@haffke.me>
2025-07-06 19:56:46 +0200

    chore(nvim): bump telescope -> nightly
    Latest release was 2024.

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -46,7 +46,6 @@ return {
     -- tools
     {
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.5",
         dependencies = {
             "nvim-lua/plenary.nvim",
             { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
