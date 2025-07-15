1f6aca24a0226d47f73e27dd17d15844b636aee2
Lasse Haffke <lasse@haffke.me>
2025-07-15 12:12:45 +0200

    fix(nvim): remove markdown.nvim
    This plugin has errors with the new nvim version

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,15 +1,5 @@
 return {
     {
-        "tadmccorkle/markdown.nvim",
-        ft = "markdown", -- or 'event = "VeryLazy"'
-        opts = {
-            mappings = false,
-            on_attach = function(bufnr)
-                require("mappings").setup_markdown_mappings(bufnr)
-            end,
-        },
-    },
-    {
         "HakonHarnes/img-clip.nvim",
         event = "VeryLazy",
         opts = {
