e67e263c751dc97dd9b5f860ddcf56c1ae362cac
Lasse Haffke <lasse@haffke.me>
2025-07-25 14:27:29 +0200

    fix(bash): remove outdated wslvliew helper

--- bash/aliases/wsl_aliases.sh
+++ bash/aliases/wsl_aliases.sh
@@ -3,7 +3,5 @@
 #WSL-specific aliases
 alias docker="docker.exe"
 alias docker-compose="docker-compose.exe"
-alias start="wslview"
 alias vs="start *.sln"
-alias xdg-open=wslview
 alias copy=win32yank.exe
