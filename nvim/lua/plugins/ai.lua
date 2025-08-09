5b00677a248bcc9f83ae4f18a90558bfe62ca0cc
Lasse Haffke <lasse@haffke.me>
2025-08-09 16:58:21 +0200

    fix(nvim-ai): replace codecompanion with copilotchat

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,29 +1,17 @@
 return {
     {
-
-        "olimorris/codecompanion.nvim",
+        "CopilotC-Nvim/CopilotChat.nvim",
+        branch = "main",
+        dependencies = {
+            { "nvim-lua/plenary.nvim" },
+        },
         opts = {
-            extensions = {
-                mcphub = {
-                    callback = "mcphub.extensions.codecompanion",
-                    opts = {
-                        show_result_in_chat = true, -- Show mcp tool results in chat
-                        make_vars = true, -- Convert resources to #variables
-                        make_slash_commands = true, -- Add prompts as /slash commands
-                    },
-                },
-            },
+            debug = true,
         },
         config = function(_, opts)
-            require("codecompanion").setup(opts)
-            require("mappings").setup_code_companion_mappings()
+            require("CopilotChat").setup(opts)
+            require("mappings").setup_copilot_chat_mappings()
         end,
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "github/copilot.vim",
-            "nvim-treesitter/nvim-treesitter",
-            "ravitemer/mcphub.nvim",
-        },
     },
     {
         "ravitemer/mcphub.nvim",
