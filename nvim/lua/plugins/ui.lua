8d558a3702d35872da8b818fd00434b38fea2611
Lasse Haffke <lasse@haffke.me>
2025-08-19 20:51:49 +0200

    feat(nvim-ai): install render-markdown.nvim for better highlighting

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -25,4 +25,19 @@ return {
             },
         },
     },
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+        dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {
+            enabled = false,
+            completions = {
+                blink = {
+                    enabled = true,
+                },
+            },
+            file_types = { "markdown", "copilot-chat" },
+        },
+    },
 }
