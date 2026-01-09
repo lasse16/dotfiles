f3180f5c95618d6735a9827ecd451c903ea11d97
Lasse Haffke <lasse@haffke.me>
2026-01-09 11:08:19 +0100

    feat(bash-jira): extend jira authenticator for jira.nvim
    These new environment variables are used by
    <https://github.com/letieu/jira.nvim>.

--- bash/helpers/authenticate-jira.bash
+++ bash/helpers/authenticate-jira.bash
@@ -2,6 +2,8 @@
 authenticate-jira() {
     local api_token=$(bw get notes "JIRA LUFTHANSA SYSTEMS PAT TOKEN")
     export JIRA_API_TOKEN="$api_token"
+    export JIRA_TOKEN="$api_token"
+    export JIRA_AUTH_TYPE="pat"
 }
 
 export -f authenticate-jira
