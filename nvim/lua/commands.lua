8814db2a0facf8798a3e619ff1d8bfb11a685e58
Lasse Haffke <lasse@haffke.me>
2026-01-27 14:41:24 +0100

    feat(nvim-commands): create formatting commands

--- nvim/lua/commands.lua
+++ nvim/lua/commands.lua
@@ -114,4 +114,25 @@ commands.snacks = {
     },
 }
 
+---@type Command[]
+commands.formatting = {
+    {
+        name = "Format",
+        cmd = function(args)
+            local range = nil
+            if args.count ~= -1 then
+                local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+                range = {
+                    start = { args.line1, 0 },
+                    ["end"] = { args.line2, end_line:len() },
+                }
+            end
+            require("conform").format({ async = true, range = range })
+        end,
+        opts = {
+            desc = "Format the current buffer",
+            range = true,
+        },
+    },
+}
 return commands
