440934c086c400468b0d5be770bc014d38f2d66d
Lasse Haffke <lasse@haffke.me>
2026-01-27 17:35:02 +0100

    feat(nvim-formatting): give option to disable auto-formatting
    Set either vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat to disable auto-formatting globally or buffer-local

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -12,11 +12,13 @@ return {
         default_format_opts = {
             lsp_format = "fallback",
         },
-        format_on_save = {
-            timeout_ms = 500,
-        },
+        format_on_save = function(bufnr)
+            -- Disable with a global or buffer-local variable
+            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                return nil
+            end
+            return { timeout_ms = 500, lsp_format = "fallback" }
+        end,
         log_level = vim.log.levels.ERROR,
-        notify_on_error = true,
-        notify_no_formatters = true,
     },
 }
