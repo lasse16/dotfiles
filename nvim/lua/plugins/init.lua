b2ff4556da1882f222a991dc5f30249f09f17cf0
Lasse Haffke <lasse@haffke.me>
2026-01-28 14:33:37 +0100

    chore(nvim): bump neovim/nvim-lspconfig 2.5.0 -> master

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,6 +1,6 @@
 return {
     -- core neovim features
-    { "neovim/nvim-lspconfig", tag = "v2.5.0" },
+    { "neovim/nvim-lspconfig" },
     {
         "nvim-treesitter/nvim-treesitter",
         branch = "main",
