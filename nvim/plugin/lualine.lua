029c037c5df87bcf900124e921d399e738328edf
Lasse Haffke <lasse@haffke.me>
2026-01-28 17:40:29 +0100

    fix(nvim-lualine): drop schema section from lualine

--- nvim/plugin/lualine.lua
+++ nvim/plugin/lualine.lua
@@ -3,15 +3,6 @@ require("lualine").setup({
     sections = {
         lualine_c = {
             "filename",
-            {
-                function()
-                    local schema = require("schema-companion.context").get_buffer_schema()
-                    return ("Schema: %s"):format(schema.name)
-                end,
-                cond = function()
-                    return package.loaded["schema-companion"] ~= nil
-                end,
-            },
         },
     },
 })
