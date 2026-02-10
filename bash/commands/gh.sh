bc712ef68bb1ca6d455a866020b8968a37a1211e
Lasse Haffke <lasse@haffke.me>
2026-02-10 12:28:22 +0100

    feat(bash): hook up new release-sha helper as part of gh cli

--- bash/commands/gh.sh
+++ bash/commands/gh.sh
@@ -3,6 +3,9 @@
 gh() {
     if [[ "$1" == "repo" ]] && [[ "$2" == "select" ]]; then 
         gh-repo-select
+    elif [[ "$1" == "release" ]] && [[ "$2" == "sha" ]]; then
+        shift 2
+        gh-release-sha
 	else
 		command gh "$@"
 	fi
