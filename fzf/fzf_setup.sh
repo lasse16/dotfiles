f5aeadcfbb7b1cd0bd1f64d6a74bdf747e01e717
Lasse Haffke <lasse@haffke.me>
2026-02-15 00:49:44 +0100

    fix(fzf): delete outdated pkgsearch utility
    This was used back in my Ubuntu days, but I never really used it so now
    it can go live on in my history.

--- fzf/fzf_setup.sh
+++ fzf/fzf_setup.sh
@@ -4,15 +4,12 @@ export DOTFILES_FZF=${DOTFILES_FZF:="$DOTFILES/fzf"}
 export FZF_DEFAULT_COMMAND='fd --hidden -I -E ".git"'
 
 # Uncomment when sourcing directly 
-source "$DOTFILES_FZF/marks-fzf.sh"
-source "$DOTFILES_FZF/pkgsearch-fzf.sh"
+source "$DOTFILES_FZF/general/marks.bash"
 source "$DOTFILES_FZF/tmux/url-runner.sh"
 source "$DOTFILES_FZF/tmux/fzf-tmux.sh"
 source "$DOTFILES_FZF/tmux/list-keys.sh"
 source "$DOTFILES_FZF/key-bindings.bash"
-source "$DOTFILES_FZF/interactive-ripgrep.bash"
 source "$DOTFILES_FZF/github/repo-select.bash"
-source "$DOTFILES_FZF/history-edit.sh"
 
 # Add subcommands by sourcing files with specific names
 pathadd "$DOTFILES_FZF/kubernetes/"
