98d36566f4174a8755f920f4d35142dcc79d35a3
Lasse Haffke <lasse@haffke.me>
2026-02-25 12:38:02 +0100

    fix(jira-authenticate): change auth_type to newer `bearer`

--- bash/helpers/authenticate-jira.bash
+++ bash/helpers/authenticate-jira.bash
@@ -3,7 +3,7 @@ authenticate-jira() {
     local api_token=$(bw get notes "JIRA LUFTHANSA SYSTEMS PAT TOKEN")
     export JIRA_API_TOKEN="$api_token"
     export JIRA_TOKEN="$api_token"
-    export JIRA_AUTH_TYPE="pat"
+    export JIRA_AUTH_TYPE="bearer"
 }
 
 export -f authenticate-jira
